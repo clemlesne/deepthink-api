362ed78b156f014456a29545a216b9e85ed892ca
Clémence Lesné <clemence@lesne.pro>
2025-03-10 00:15:37 +0100

    test: Tokens usage in response

--- tests/sdk_litellm.py
+++ tests/sdk_litellm.py
@@ -1,6 +1,6 @@
 import pytest
 from litellm import CustomStreamWrapper, acompletion
-from litellm.types.utils import Choices, ModelResponse, StreamingChoices
+from litellm.types.utils import Choices, ModelResponse, StreamingChoices, Usage
 
 # Dummy message
 MESSAGES = [
@@ -23,12 +23,20 @@ async def test_litellm_sync():
 
     # Validate response
     assert len(res.choices) == 1
-    assert res.choices[0].finish_reason == "stop"
-    assert isinstance(res.choices[0], Choices)
-    assert res.choices[0].message.content is not None
 
-    # Debug
-    print(res.choices[0].message.content)  # noqa: T201
+    # Validate choice
+    choice = res.choices[0]
+    assert isinstance(choice, Choices)
+    assert choice.finish_reason == "stop"
+    assert choice.message.content is not None
+    print(choice.message.content)  # noqa: T201
+
+    # Validate usage
+    usage = res.get("usage")
+    assert isinstance(usage, Usage)
+    assert usage.prompt_tokens > 0
+    assert usage.completion_tokens > 0
+    assert usage.total_tokens == usage.prompt_tokens + usage.completion_tokens
 
 
 @pytest.mark.asyncio
@@ -44,13 +52,25 @@ async def test_litellm_stream():
 
     # Consume stream
     async for delta in res:
+        last_delta = False
+
         # Validate response
         assert len(delta.choices) == 1
-        assert isinstance(delta.choices[0], StreamingChoices)
-        if delta.choices[0].delta.content is None:
-            assert delta.choices[0].finish_reason == "stop"
-        else:
-            assert delta.choices[0].finish_reason is None
 
-        # Debug
-        print(delta.choices[0].delta.content)  # noqa: T201
+        # Validate choice
+        choice = delta.choices[0]
+        assert isinstance(choice, StreamingChoices)
+        if choice.delta.content is None:
+            assert choice.finish_reason == "stop"
+            last_delta = True
+        else:
+            assert choice.finish_reason is None
+        print(choice.delta.content)  # noqa: T201
+
+        # Validate usage
+        usage = delta.get("usage", None)
+        if last_delta:
+            pass
+            # TODO: Validate usage, "usage" field is never present in the object
+        else:
+            assert usage is None
--- tests/sdk_openai.py
+++ tests/sdk_openai.py
@@ -29,12 +29,20 @@ async def test_openai_sync():
 
         # Validate response
         assert len(res.choices) == 1
-        assert res.choices[0].finish_reason == "stop"
-        assert isinstance(res.choices[0], SyncChoice)
-        assert res.choices[0].message.content is not None
 
-        # Debug
-        print(res.choices[0].message.content)  # noqa: T201
+        # Validate choice
+        choice = res.choices[0]
+        assert isinstance(choice, SyncChoice)
+        assert choice.finish_reason == "stop"
+        assert choice.message.content is not None
+        print(choice.message.content)  # noqa: T201
+
+        # Validate usage
+        usage = res.usage
+        assert usage is not None
+        assert usage.prompt_tokens > 0
+        assert usage.completion_tokens > 0
+        assert usage.total_tokens == usage.prompt_tokens + usage.completion_tokens
 
 
 @pytest.mark.asyncio
@@ -52,13 +60,29 @@ async def test_openai_stream():
 
         # Consume stream
         async for delta in res:
+            last_delta = False
+
             # Validate response
             assert len(delta.choices) == 1
-            assert isinstance(delta.choices[0], StreamChoice)
-            if delta.choices[0].delta.content is None:
-                assert delta.choices[0].finish_reason == "stop"
-            else:
-                assert delta.choices[0].finish_reason is None
 
-            # Debug
-            print(delta.choices[0].delta.content)  # noqa: T201
+            # Validate choice
+            choice = delta.choices[0]
+            assert isinstance(choice, StreamChoice)
+            if choice.delta.content is None:
+                assert choice.finish_reason == "stop"
+                last_delta = True
+            else:
+                assert choice.finish_reason is None
+            print(choice.delta.content)  # noqa: T201
+
+            # Validate usage
+            usage = delta.usage
+            if last_delta:
+                assert usage is not None
+                assert usage.prompt_tokens > 0
+                assert usage.completion_tokens > 0
+                assert (
+                    usage.total_tokens == usage.prompt_tokens + usage.completion_tokens
+                )
+            else:
+                assert usage is None
